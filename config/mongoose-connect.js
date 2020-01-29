bdf6b68d824ca8d484076f2f14b11fc3c71230be
Jailson de Souza <jailson.souza@CTNDELLG565K03.pampa.compasso>
2020-01-29 17:06:40 -0300

    full 2

--- config/mongoose-connect.js
+++ config/mongoose-connect.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose')
+const srcCon = 'mongodb://localhost/postapp'
+
+// const strCon = 'mongodb+srv://admin:admin@cluster0-sv2jx.mongodb.net/postapp?retryWrites=true&w=majority'
 
 module.exports = () => {
-  mongoose.connect('mongodb+srv://admin:admin@cluster0-sv2jx.mongodb.net/postapp?retryWrites=true&w=majority', {
+  mongoose.connect(srcCon, {
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
