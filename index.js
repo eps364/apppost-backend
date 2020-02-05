1c252bd0ebc5df6f717197074ed6448157b0c693
Jailson de Souza <jailson.souza@CTNDELLG565K03.pampa.compasso>
2020-02-05 09:10:17 -0300

    configurando a porta .env 2

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('./config/custom-express')
 
-const porta = process.env.PORT || 3000;
+const porta = process.env.PORT || 3001;
 
 express.listen(porta, () => console.log(`Servidor rodando na porta: ${porta}!`))
