e4ce5a6ecc816180510f38ccec93b67d5e544f3d
Emerson Pereira <pereiradasilva.emerson@gmail.com>
2020-02-05 11:53:01 -0300

    Fix: Dotenv PORT

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('./config/custom-express')
+require('dotenv/config')
 
 const porta = process.env.APP_PORT || 3000
+
 express.listen(porta, () => console.log(`Servidor rodando na porta: ${porta}!`))
