364a3dd93278d81c889fb7a3b840d8fd7cd05ce6
Emerson Pereira <pereiradasilva.emerson@gmail.com>
2020-02-11 11:01:36 -0300

    Fix: Validações User

--- app/routes/UserRoute.js
+++ app/routes/UserRoute.js
@@ -77,50 +77,12 @@ module.exports = app => {
                 .then(success => res.status(200).json(success))
                 .catch(error => res.status(500).json(error))
         })
-        .put([
-                check('nome')
-                .isLength({
-                    min: 3
-                })
-                .withMessage('Preencha com mais de 3 letras'),
+        .put((req, res) => {
 
-                check('cpf')
-                .isNumeric()
-                .isLength({
-                    min: 11,
-                    max: 11
-                })
-                .withMessage('Digite um cpf valido, apenas numeros, sem ponto e sem hifem.'),
+            req.body.id = req.params.id;
 
-                check('email')
-                .isEmail()
-                .withMessage('Digite um email no formato correto.'),
-
-                check('senha')
-                .isLength({
-                    min: 4
-                })
-                .withMessage('Senha incompativel, digite outra e maior que 4 digitos'),
-                check('curso_id')
-                .isEmpty()
-                .withMessage('Selecione um dos cursos corretamente'),
-                check('caracteristica')
-                .isEmpty()
-                .withMessage('Selecione uma das categorias.')
-
-            ],
-            (req, res) => {
-                const errors = validationResult(req);
-                if (!errors.isEmpty()) {
-                    return res.status(422).json({
-                        errors: errors.array()
-                    });
-                }
-
-                req.body.id = req.params.id;
-
-                usercontroller.update(req.body)
-                    .then(success => res.status(200).json(success))
-                    .catch(error => res.status(500).json(error))
-            })
+            usercontroller.update(req.body)
+                .then(success => res.status(200).json(success))
+                .catch(error => res.status(500).json(error))
+        })
 }
